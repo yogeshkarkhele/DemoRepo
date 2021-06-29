4908043212a07e18928bbdbb5246c5ec78d97b8f
ykarkhele <ykarkhele@host.docker.internal>
2021-06-29 08:32:18 +0530

    classb added

---
+++ src/session6/ClassB.java
@@ -0,0 +1,15 @@
+package session6;
+
+public class ClassB extends ClassA{
+
+	public static void main(String[] args) {
+
+		ClassB classB = new ClassB();
+		classB.methodA();
+		classB.methodB();
+
+	}
+	
+	
+	
+}
